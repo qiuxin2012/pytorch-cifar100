ccd9688f17b8af5682923ccd83e9f0e28c7d2f38
qiuxin2012 <qiuxin2012cs@gmail.com>
2020-06-17 12:58:34 +0800

    az

--- train.py
+++ train.py
@@ -6,118 +6,38 @@
 author baiyu
 """
 
-import os
-import sys
 import argparse
-from datetime import datetime
 
-import numpy as np
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torchvision
-import torchvision.transforms as transforms
-
-from torch.utils.data import DataLoader
-#from dataset import *
-from torch.autograd import Variable
-
-from tensorboardX import SummaryWriter
+from bigdl.optim.optimizer import *
+from zoo.pipeline.api.torch import TorchModel, TorchLoss
+from zoo.pipeline.estimator import *
+from zoo.common.nncontext import *
+from zoo.feature.common import FeatureSet
+from zoo.pipeline.api.keras.metrics import Accuracy
 
 from conf import settings
 from utils import get_network, get_training_dataloader, get_test_dataloader, WarmUpLR
 
-def train(epoch):
-
-    net.train()
-    for batch_index, (images, labels) in enumerate(cifar100_training_loader):
-        if epoch <= args.warm:
-            warmup_scheduler.step()
-
-        images = Variable(images)
-        labels = Variable(labels)
-
-        labels = labels.cuda()
-        images = images.cuda()
-
-        optimizer.zero_grad()
-        outputs = net(images)
-        loss = loss_function(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        n_iter = (epoch - 1) * len(cifar100_training_loader) + batch_index + 1
-
-        last_layer = list(net.children())[-1]
-        for name, para in last_layer.named_parameters():
-            if 'weight' in name:
-                writer.add_scalar('LastLayerGradients/grad_norm2_weights', para.grad.norm(), n_iter)
-            if 'bias' in name:
-                writer.add_scalar('LastLayerGradients/grad_norm2_bias', para.grad.norm(), n_iter)
-
-        print('Training Epoch: {epoch} [{trained_samples}/{total_samples}]\tLoss: {:0.4f}\tLR: {:0.6f}'.format(
-            loss.item(),
-            optimizer.param_groups[0]['lr'],
-            epoch=epoch,
-            trained_samples=batch_index * args.b + len(images),
-            total_samples=len(cifar100_training_loader.dataset)
-        ))
-
-        #update training loss for each iteration
-        writer.add_scalar('Train/loss', loss.item(), n_iter)
-
-    for name, param in net.named_parameters():
-        layer, attr = os.path.splitext(name)
-        attr = attr[1:]
-        writer.add_histogram("{}/{}".format(layer, attr), param, epoch)
-
-def eval_training(epoch):
-    net.eval()
-
-    test_loss = 0.0 # cost function error
-    correct = 0.0
-
-    for (images, labels) in cifar100_test_loader:
-        images = Variable(images)
-        labels = Variable(labels)
-
-        images = images.cuda()
-        labels = labels.cuda()
-
-        outputs = net(images)
-        loss = loss_function(outputs, labels)
-        test_loss += loss.item()
-        _, preds = outputs.max(1)
-        correct += preds.eq(labels).sum()
-
-    print('Test set: Average loss: {:.4f}, Accuracy: {:.4f}'.format(
-        test_loss / len(cifar100_test_loader.dataset),
-        correct.float() / len(cifar100_test_loader.dataset)
-    ))
-    print()
-
-    #add informations to tensorboard
-    writer.add_scalar('Test/Average loss', test_loss / len(cifar100_test_loader.dataset), epoch)
-    writer.add_scalar('Test/Accuracy', correct.float() / len(cifar100_test_loader.dataset), epoch)
-
-    return correct.float() / len(cifar100_test_loader.dataset)
+import torch.nn as nn
 
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
     parser.add_argument('-net', type=str, required=True, help='net type')
-    parser.add_argument('-gpu', type=bool, default=True, help='use gpu or not')
-    parser.add_argument('-w', type=int, default=2, help='number of workers for dataloader')
+    parser.add_argument('-w', type=int, default=0, help='number of workers for dataloader')
     parser.add_argument('-b', type=int, default=128, help='batch size for dataloader')
     parser.add_argument('-s', type=bool, default=True, help='whether shuffle the dataset')
     parser.add_argument('-warm', type=int, default=1, help='warm up training phase')
     parser.add_argument('-lr', type=float, default=0.1, help='initial learning rate')
+    parser.add_argument('-path', type=str, default='./data', help='datapath')
     args = parser.parse_args()
+    net = get_network(args, use_gpu=False)
+    print(net)
+    loss_function = nn.CrossEntropyLoss()
 
-    net = get_network(args, use_gpu=args.gpu)
-        
     #data preprocessing:
     cifar100_training_loader = get_training_dataloader(
+        args.path,
         settings.CIFAR100_TRAIN_MEAN,
         settings.CIFAR100_TRAIN_STD,
         num_workers=args.w,
@@ -126,48 +46,52 @@ if __name__ == '__main__':
     )
     
     cifar100_test_loader = get_test_dataloader(
+        args.path,
         settings.CIFAR100_TRAIN_MEAN,
         settings.CIFAR100_TRAIN_STD,
         num_workers=args.w,
         batch_size=args.b,
         shuffle=args.s
     )
-    
-    loss_function = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
-    train_scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=settings.MILESTONES, gamma=0.2) #learning rate decay
+
+    # init on yarn when HADOOP_CONF_DIR and ZOO_CONDA_NAME is provided.
+    if os.environ.get('HADOOP_CONF_DIR') is None:
+        sc = init_spark_on_local(cores=1, conf={"spark.driver.memory": "20g"})
+    else:
+        num_executors = 2
+        num_cores_per_executor = 4
+        hadoop_conf_dir = os.environ.get('HADOOP_CONF_DIR')
+        zoo_conda_name = os.environ.get('ZOO_CONDA_NAME')  # The name of the created conda-env
+        sc = init_spark_on_yarn(
+            hadoop_conf=hadoop_conf_dir,
+            conda_name=zoo_conda_name,
+            num_executor=num_executors,
+            executor_cores=num_cores_per_executor,
+            executor_memory="2g",
+            driver_memory="10g",
+            driver_cores=1,
+            spark_conf={"spark.rpc.message.maxSize": "1024",
+                        "spark.task.maxFailures":  "1",
+                        "spark.driver.extraJavaOptions": "-Dbigdl.failure.retryTimes=1"})
+
     iter_per_epoch = len(cifar100_training_loader)
-    warmup_scheduler = WarmUpLR(optimizer, iter_per_epoch * args.warm)
-    checkpoint_path = os.path.join(settings.CHECKPOINT_PATH, args.net, settings.TIME_NOW)
+    warmup_delta = args.lr / (iter_per_epoch * args.warm)
+    # iteration_per_epoch = int(math.ceil(float(len(cifar100_training_loader)) / args.b))
+    zoo_lrSchedule = SequentialSchedule(iter_per_epoch)
+    zoo_lrSchedule.add(Warmup(warmup_delta), iter_per_epoch * args.warm)
+    zoo_lrSchedule.add(MultiStep([iter_per_epoch * 60, iter_per_epoch * 120, iter_per_epoch * 160], 0.2), iter_per_epoch * 200)
+    zoo_optim = SGD(learningrate=warmup_delta, learningrate_decay=0.0, weightdecay=5e-4,
+                momentum=0.9, dampening=0.0, nesterov=False,
+                leaningrate_schedule=zoo_lrSchedule)
 
-    #use tensorboard
-    if not os.path.exists(settings.LOG_DIR):
-        os.mkdir(settings.LOG_DIR)
-    writer = SummaryWriter(log_dir=os.path.join(
-            settings.LOG_DIR, args.net, settings.TIME_NOW))
-    input_tensor = torch.Tensor(12, 3, 32, 32).cuda()
-    writer.add_graph(net, Variable(input_tensor, requires_grad=True))
-
-    #create checkpoint folder to save model
-    if not os.path.exists(checkpoint_path):
-        os.makedirs(checkpoint_path)
-    checkpoint_path = os.path.join(checkpoint_path, '{net}-{epoch}-{type}.pth')
-
-    best_acc = 0.0
-    for epoch in range(1, settings.EPOCH):
-        if epoch > args.warm:
-            train_scheduler.step(epoch)
-
-        train(epoch)
-        acc = eval_training(epoch)
-
-        #start to save best performance model after learning rate decay to 0.01 
-        if epoch > settings.MILESTONES[1] and best_acc < acc:
-            torch.save(net.state_dict(), checkpoint_path.format(net=args.net, epoch=epoch, type='best'))
-            best_acc = acc
-            continue
-
-        if not epoch % settings.SAVE_EPOCH:
-            torch.save(net.state_dict(), checkpoint_path.format(net=args.net, epoch=epoch, type='regular'))
-
-    writer.close()
+    zoo_model = TorchModel.from_pytorch(net)
+    zoo_loss = TorchLoss.from_pytorch(loss_function)
+    zoo_estimator = Estimator(zoo_model, optim_methods=zoo_optim)
+    train_featureset = FeatureSet.pytorch_dataloader(cifar100_training_loader)
+    test_featureset = FeatureSet.pytorch_dataloader(cifar100_test_loader)
+    from bigdl.optim.optimizer import MaxEpoch, EveryEpoch
+    zoo_estimator.train_minibatch(train_featureset, zoo_loss,
+                                  end_trigger=MaxEpoch(settings.EPOCH),
+                                  checkpoint_trigger=EveryEpoch(),
+                                  validation_set=test_featureset,
+                                  validation_method=[Accuracy()])
